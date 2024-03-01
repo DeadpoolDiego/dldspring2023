a825fcb091dc21b504e31a3ea1f82f8d634447a5
DeadpoolDiego <76859022+DeadpoolDiego@users.noreply.github.com>
2024-03-01 11:41:59 -0600

    Add files via upload

--- Lab2/SV/des_stub.sv
+++ Lab2/SV/des_stub.sv
@@ -27,16 +27,193 @@ module GenerateKeys (Key, SubKey1, SubKey2, SubKey3, SubKey4,
    output logic [47:0] SubKey14;
    output logic [47:0] SubKey15;
    output logic [47:0] SubKey16;
+   logic [27:0] c1;
+   logic [27:0] d1;
+   logic [27:0] c2;
+   logic [27:0] d2;
+   logic [27:0] c3;
+   logic [27:0] d3;
+   logic [27:0] c4;
+   logic [27:0] d4;
+   logic [27:0] c5;
+   logic [27:0] d5;
+   logic [27:0] c6;
+   logic [27:0] d6;
+   logic [27:0] c7;
+   logic [27:0] d7;
+   logic [27:0] c8;
+   logic [27:0] d8;
+   logic [27:0] c9;
+   logic [27:0] d9;
+   logic [27:0] c10;
+   logic [27:0] d10;
+   logic [27:0] c11;
+   logic [27:0] d11;
+   logic [27:0] c12;
+   logic [27:0] d12;
+   logic [27:0] c13;
+   logic [27:0] d13;
+   logic [27:0] c14;
+   logic [27:0] d14;
+   logic [27:0] c15;
+   logic [27:0] d15;
+   logic [27:0] c16;
+   logic [27:0] d16;
+
+		PC1 I(key,c1,d1);
+
+
+	assign c2 = {c1[26:0],c1[27]};
+	assign d2 = {d1[26:0],d1[27]};
+	PC2 II(c2, d2, subkey1);
+
+
+	assign c3 = {c2[26:0],c2[27]};
+	assign d3 = {d2[26:0],d2[27]};
+	PC2 III(c3,d3,subkey2);
+
+
+	assign c4 = {c3[25:0],c3[27:26]};
+	assign d4 = {d3[25:0],d3[27:26]};
+	PC2 IV(c4,s4,subkey4);
+
+
+	assign c5 = {c4[25:0],c4[27:26]};
+	assign d5 = {d4[25:0],d4[27:26]};
+	PC2 V(c5,d5,subkey5);
+
+
+	assign c6 = {c5[25:0],c5[27:26]};
+	assign d6 = {d5[25:0],d5[27:26]};
+	PC2 VI(c6,d6,subkey6);
+
+
+	assign c7 = {c6[25:0],c6[27:26]};
+	assign d7 = {d6[25:0],d6[27:26]};
+	PC2 VII(c7,d7,subkey7);
+
+
+	assign c8 = {c7[25:0],c7[27:26]};
+	assign d8 = {d7[25:0],d7[27:26]};
+	PC2 VIII(c8,d8,subkey8);
+
+
+	assign c9 = {c8[25:0],c8[27:26]};
+	assign d9 = {d8[25:0],d8[27:26]};
+	PC2 IX(c9,d9,subkey9);
+
+
+	assign c10 = {c9[26:0],c9[27]};
+	assign d10 = {d9[26:0],d9[27]};
+	PC2 X(c10,d10,subkey10);
+
+
+	assign c11 = {c10[25:0],c10[27]};
+	assign d11 = {d10[25:0],d10[27]};
+	PC2 XI(c11,d11,subkey11);
+
+
+	assign c12 = {c11[25:0],c11[27]};
+	assign d12 = {d11[25:0],d11[27:26]};
+	PC2 XII(c12,d12,subkey12);
+
+
+	assign c13 = {c12[25:0],c12[27:26]};
+	assign d13 = {d12[25:0],d12[27:26]};
+	PC2 XIII(c13,d13,subkey13);
+
+
+	assign c14 = {c13[25:0],c13[27:26]};
+	assign d14 = {d13[25:0],d13[27:26]};
+	PC2 XIV(c14,d14,subkey14);
+
+
+	assign c15 = {c14[25:0],c14[27:26]};
+	assign d15 = {d14[25:0],d14[27:26]};
+	PC2 XV(c15,d15,subkey15);
+
+
+	assign c16 = {c15[26:0],c15[27]};
+	assign d16 = {d15[26:0],d15[27]};
+	PC2 XVI(c16,d16,subkey16);
 
 endmodule // GenerateKeys
 
 module PC1 (key, left_block, right_block);
 
    input logic [63:0]  key;
-   output logic [27:0] left_block;
-   output logic [27:0] right_block;
+   	output logic [27:0] c1;
+	output logic [27:0] d1;
+	
+
 
    logic [55:0]        out_block;
+   //LEFT BLOCK
+	assign c1[27] = key[64-57];
+	assign c1[26] = key[64-49];
+	assign c1[25] = key[64-41];
+	assign c1[24] = key[64-33];
+	assign c1[23] = key[64-25];
+	assign c1[22] = key[64-17];
+	assign c1[21] = key[64-9];
+	assign c1[20] = key[64-1];
+	assign c1[19] = key[64-58];
+	assign c1[18] = key[64-50];
+	assign c1[17] = key[64-42];
+	assign c1[16] = key[64-34];
+	assign c1[15] = key[64-26];
+	assign c1[14] = key[64-18];
+	assign c1[13] = key[64-10];
+	assign c1[12] = key[64-2];
+	assign c1[11] = key[64-59];
+	assign c1[10] = key[64-51];
+	assign c1[9] = key[64-43];
+	assign c1[8] = key[64-35];
+	assign c1[7] = key[64-27];
+	assign c1[6] = key[64-19];
+	assign c1[5] = key[64-11];
+	assign c1[4] = key[64-3];
+	assign c1[3] = key[64-60];
+	assign c1[2] = key[64-52];
+	assign c1[1] = key[64-44];
+	assign c1[0] = key[64-36];
+
+   //RIGHT BLOCK
+	assign d1[27] = key[64-63];
+	assign d1[26] = key[64-55];
+	assign d1[25] = key[64-47];
+	assign d1[24] = key[64-39];
+	assign d1[23] = key[64-31];
+	assign d1[22] = key[64-23];
+	assign d1[21] = key[64-15];
+	assign d1[20] = key[64-7];
+	assign d1[19] = key[64-62];
+	assign d1[18] = key[64-54];
+	assign d1[17] = key[64-46];
+	assign d1[16] = key[64-38];
+	assign d1[15] = key[64-30];
+	assign d1[14] = key[64-22];
+	assign d1[13] = key[64-14];
+	assign d1[12] = key[64-6];
+	assign d1[11] = key[64-61];
+	assign d1[10] = key[64-53];
+	assign d1[9] = key[64-45];
+	assign d1[8] = key[64-37];
+	assign d1[7] = key[64-29];
+	assign d1[6] = key[64-21];
+	assign d1[5] = key[64-13];
+	assign d1[4] = key[64-5];
+	assign d1[3] = key[64-28];
+	assign d1[2] = key[64-20];
+	assign d1[1] = key[64-12];
+	assign d1[0] = key[64-4];
+
+
+	
+
+
+	//Shuffled Deck
+	//assign out_block[55:0] = {shiftedLeftBlock[27:0],shiftedRightBlock[27:0]};
 
 endmodule // PC1
 
@@ -860,7 +1037,24 @@ module DES (input logic [63:0] key, input logic [63:0] plaintext,
    logic [47:0] 	SubKey13, SubKey14, SubKey15, SubKey16;
 
    logic [63:0] 	ip_out;   
-   logic [63:0] 	r16_out = 64'h0;   
+   logic [63:0] 	r16_out;
+   logic [63:0] 	r15_out;
+   logic [63:0] 	r14_out;
+   logic [63:0] 	r13_out;
+   logic [63:0] 	r12_out;
+   logic [63:0] 	r11_out;
+   logic [63:0] 	r10_out;
+   logic [63:0] 	r9_out;
+   logic [63:0] 	r8_out;
+   logic [63:0] 	r7_out;
+   logic [63:0] 	r6_out;
+   logic [63:0] 	r5_out;
+   logic [63:0] 	r4_out;
+   logic [63:0] 	r3_out;
+   logic [63:0] 	r2_out;
+   logic [63:0] 	r1_out;
+
+
    
    // SubKey generation
    GenerateKeys k1 (key, SubKey1, SubKey2, SubKey3, SubKey4,
@@ -872,37 +1066,37 @@ module DES (input logic [63:0] key, input logic [63:0] plaintext,
    // Initial Permutation (IP)
    IP b1 (plaintext, ip_out);
    // round 1
-   
+   round r1 (ip_out, SubKey1, r1_out);
    // round 2
-   
+   round r2 (r1_out, SubKey2, r2_out);
    // round 3
-   
+   round r3 (r2_out, SubKey3, r3_out);
    // round 4
-   
+   round r4 (r3_out, SubKey4, r4_out);
    // round 5
-   
+   round r5 (r4_out, SubKey5, r5_out);
    // round 6
-   
+   round r6 (r5_out, SubKey6, r6_out);
    // round 7
-   
+   round r7 (r6_out, SubKey7, r7_out);
    // round 8
-   
+   round r8 (r7_out, SubKey8, r8_out);
    // round 9
-   
+   round r9 (r8_out, SubKey9, r9_out);
    // round 10
-   
+   round r10 (r9_out, SubKey10, r10_out);
    // round 11
-   
+   round r11 (r10_out, SubKey11, r11_out);
    // round 12
-   
+   round r12 (r11_out, SubKey12, r12_out);
    // round 13
-   
+   round r13 (r12_out, SubKey13, r13_out);
    // round 14
-   
+   round r14 (r13_out, SubKey14, r14_out);
    // round 15
-   
+   round r15 (r14_out, SubKey15, r15_out);
    // round 16
-
+   round r16 (r15_out, SubKey16, r16_out);
    // Final Permutation (IP^{-1}) (swap output of round16)
    FP FP({r16_out[31:0], r16_out[63:32]}, ciphertext);
    
